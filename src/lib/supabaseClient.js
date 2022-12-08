16b8c2030ee52fdf45970b0b07d0b444ec2c75fa
Jose Arturo <arturotoll@gmail.com>
2022-12-07 19:39:31 -0500

    asdfasd

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(
+    "https://hbcdfrawxofhehvmbzde.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiY2RmcmF3eG9maGVodm1iemRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk0MjU3MDUsImV4cCI6MTk4NTAwMTcwNX0.5jr8FoJxO0sD93Oual8Eq8BucWSmfC4Fvy7LfCOILkk");
 
